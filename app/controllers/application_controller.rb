a12cff2ecbf76322eafca7d3a68dbcb31b6d282f
ytakegawa <ytkgw1986@gmail.com>
2021-12-15 15:33:00 +0900

    Add: User information edit page, Change redirect to sign_up and login
    ユーザ情報編集ページの追加、サインアップとログインのリダイレクト先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :adress, :self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :adress, :self_introduction])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, 　controllers: { registrations: 'users/registrations' }
-  resources :users, only: [:index, :show]
-  #get 'users/:id', to: 'users#show'
-  # get 'users/new', to: 'users#new'
-  # get 'users/index', to: 'users#index'
-  resources :books
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'users#show', as: 'user_profile'
+
+  resources :books
   root to: 'books#index'
-  #devise_for :users, controllers: { registrations: 'users/registrations' }
 end
