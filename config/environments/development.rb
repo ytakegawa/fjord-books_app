d554a50018a8869e55c0632f034f58f810877acd
ytakegawa <ytkgw1986@gmail.com>
2021-12-17 22:32:31 +0900

    Add: Email sender setting for reset password instructions
    パスワード再設定のメール送信設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,9 +34,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => 'fjordbootcamp33email@gmail.com',
+    :password => 'vtpb jwdl lpdj einc',
+    :authentication => 'login',
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -74,5 +84,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
