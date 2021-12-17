9d543a8cbe8414d2f34aead59fc3e972fcb1e394
ytakegawa <ytkgw1986@gmail.com>
2021-12-17 22:39:51 +0900

    Fix: Delete viewing permissions for the user/show page
    ユーザ詳細画面の閲覧権限を削除した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    unless @user.id == current_user.id
-      redirect_to users_path
-    end
   end
 end
