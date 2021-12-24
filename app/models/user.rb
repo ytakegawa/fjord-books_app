33e59fb9867dfb88f4e020aec1d6b5387959871e
ytakegawa <ytkgw1986@gmail.com>
2021-12-24 14:39:33 +0900

    Add: Function to upload avatar images
    ユーザのアイコン画像のアップロード機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validate :image_file_type_check
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  private
+
+  def image_file_type_check
+    return unless avatar.attached?
+    unless image_file?
+      errors.add(:avatar, I18n.t('errors.messages.file_type_not_image'))
+    end
+  end
+
+  def image_file?
+    %[image/jpg image/jpeg image/gif image/png].include?(avatar.content_type)
+  end
 end
