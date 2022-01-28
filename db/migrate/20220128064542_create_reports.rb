774e0dd9f918c53d03b7b5cf07112255c5febec8
ytakegawa <ytkgw1986@gmail.com>
2022-01-28 15:51:14 +0900

    Add: Report Model
    reportモデルの作成

---
+++ db/migrate/20220128064542_create_reports.rb
@@ -0,0 +1,12 @@
+class CreateReports < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reports do |t|
+      t.string :title
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :reports, [:user_id, :created_at]
+  end
+end
