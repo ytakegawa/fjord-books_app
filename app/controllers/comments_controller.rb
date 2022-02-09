1cb2c7e070dd82d1a9267b473ce2cd3785c5a0dc
ytakegawa <ytkgw1986@gmail.com>
2022-02-09 15:20:35 +0900

    Improve: comments delete function to Report
    日報のコメント削除機能を修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
-
     respond_to do |format|
       if @comment.save
         format.html { redirect_to report_path(params[:comment][:commentable_id]), notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
@@ -15,18 +14,18 @@ class CommentsController < ApplicationController
           @books = @book.comments.order(:created_at).page(params[:page]).per(5)
         end
         @error_comment = @comment
-        format.html { render 'reports/show' } #{ redirect_to report_path(params[:comment][:commentable_id]) }
+        format.html { render 'reports/show' }
       end
     end
   end
 
   def destroy
-    binding.irb
-    @comment = Comment.find(params[:report_id])
-    @comment ||= Comment.find(params[:book_id])
+    @comment = Comment.find(params[:id])
     @comment.destroy
+    @commentable_id = params[:report_id]
+    @commentable_id ||= params[:book_id]
     respond_to do |format|
-      format.html { redirect_to report_path(params[:id]), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
+      format.html { redirect_to report_path(@commentable_id), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
     end
   end
 
