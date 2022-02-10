63012a9bf4d9f83cea7e695c18f9871c4422ed61
ytakegawa <ytkgw1986@gmail.com>
2022-02-10 11:14:15 +0900

    Refactoring: Check for code duplication, etc
    コードの重複などを見直しリファクタリングを実行

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
-    commentable_type = Report.find(@comment.commentable_id) if @comment.commentable_type == 'Report'
-    commentable_type ||= Book.find(@comment.commentable_id)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to polymorphic_path(commentable_type), notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
+        format.html { redirect_to polymorphic_path(find_content(@comment)), notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
       else
+        # HTML5の必須入力チェックが機能しなかった場合のエラー時レスポンス
         @error_comment = @comment
         case params[:comment][:commentable_type]
         when 'Report'
@@ -15,7 +14,7 @@ class CommentsController < ApplicationController
           format.html { render 'reports/show' }
         when 'Book'
           @book = Book.find(params[:book_id])
-          @books = @book.comments.order(:created_at).page(params[:page]).per(5)
+          @comments = @book.comments.order(:created_at).page(params[:page]).per(5)
           format.html { render 'books/show' }
         end
       end
@@ -24,13 +23,9 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment = Comment.find(params[:id])
-    commentable_type = Report.find(@comment.commentable_id) if @comment.commentable_type == 'Report'
-    commentable_type ||= Book.find(@comment.commentable_id)
     @comment.destroy
-    commentable_id = params[:report_id]
-    commentable_id ||= params[:book_id]
     respond_to do |format|
-      format.html { redirect_to polymorphic_path(commentable_type), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
+      format.html { redirect_to polymorphic_path(find_content(@comment)), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
     end
   end
 
@@ -38,5 +33,14 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content, :commentable_type, :commentable_id)
+  end
+
+  def find_content(comment)
+    case comment.commentable_type
+    when 'Report'
+      Report.find(comment.commentable_id)
+    when 'Book'
+      Book.find(comment.commentable_id)
+    end
   end
 end
