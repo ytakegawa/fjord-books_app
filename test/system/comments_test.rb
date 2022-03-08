3bdf69796ab27d52ce9f65c8973fa326220d1d74
ytakegawa <ytkgw1986@gmail.com>
2022-03-08 16:08:49 +0900

    Add: Comments system-test
    CommentのCRUDのシステムテストを追加

--- test/system/comments_test.rb
+++ test/system/comments_test.rb
@@ -1,9 +1,31 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class CommentsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit comments_url
-  #
-  #   assert_selector "h1", text: "Comments"
-  # end
+  setup do
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+  end
+
+  test 'ceate a comment for a book' do
+    click_on '詳細', match: :first
+
+    fill_in 'comment[content]', with: 'Bookテストコメントを入力'
+    click_button 'コメントする'
+
+    assert_text 'Bookテストコメントを入力'
+    assert_text 'コメントが投稿されました'
+  end
+
+  test 'ceate a comment for a report' do
+    click_on '日報'
+    click_on '詳細', match: :first
+
+    fill_in 'comment[content]', with: 'Reportテストコメントを入力'
+    click_button 'コメントする'
+
+    assert_text 'Reportテストコメントを入力'
+    assert_text 'コメントが投稿されました'
+  end
 end
