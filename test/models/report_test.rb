2a63235c5c492cc1366e39acf2f71e163522b924
ytakegawa <ytkgw1986@gmail.com>
2022-03-09 15:09:41 +0900

    Check: check code by robocop
    rubocopによるコードチェック

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -7,16 +7,8 @@ class ReportTest < ActiveSupport::TestCase
     carol = User.create!(email: 'carol@example.com', name: 'carol', password: 'password')
     dave = User.create!(email: 'dave@example.com', name: 'dave', password: 'password')
     carol_report = Report.create!(user_id: carol.id, title: 'テスト日報', content: '日報を作成しました')
-    dave_report = Report.create!(user_id: dave.id, title: 'テスト日報', content: '日報を作成しました')
 
     assert_not carol_report.editable?(dave)
     assert carol_report.editable?(carol)
-  end
-
-  test 'created_on' do
-    carol = User.create!(email: 'carol@example.com', name: 'carol', password: 'password')
-    carol_report = Report.create!(user_id: carol.id, title: 'テスト日報', content: '日報を作成しました')
-
-    assert Date.today == carol_report.created_on
   end
 end
